e44b86dd8c3d06340e28c855f384d1efa2cdca2f
Nokondi <hail.nothing@gmail.com>
2023-04-27 20:48:56 -0500

    scream

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,44 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [scream, setScream] = useState(true);
+
+    const toggleSwitch = () => {
+        setScream(!scream);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Switch 
+                onValueChange={toggleSwitch}
+                value={scream}
+            />
+            {scream ? (
+                <View>
+                    <Text style={styles.switchText}>Scream</Text>
+                </View>
+            ) : (
+                <View>
+                    <Text style={styles.switchText}>Gaze</Text>
+                </View>
+            )}
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
+container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+},
+switchText: {
+    color: '#fff',
+    fontSize: 24,
+}
 });
