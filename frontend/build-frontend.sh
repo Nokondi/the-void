6d26a7dfc83354055a34c998ce83154bb94d2228
Nokondi <hail.nothing@gmail.com>
2023-05-24 09:20:02 -0500

    update build script

--- frontend/build-frontend.sh
+++ frontend/build-frontend.sh
@@ -1,7 +1,7 @@
-apt upgrade
-apt update
+apt upgrade -y
+apt update -y
 apt-get install nodejs npm -y
-npm install ~/the-void/frontend
-npx expo export:web ~/the-void/frontend
+npm install ~/the-void/frontend -y
+npx expo export:web ~/the-void/frontend -y
 rm -rf /usr/share/nginx/html/*
 cp -R ~/the-void/frontend/web-build /usr/share/nginx/html
