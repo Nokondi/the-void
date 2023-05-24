27ccf05698b227e0b1929a6f10dfaeb859a31a32
Nokondi <hail.nothing@gmail.com>
2023-05-24 13:12:03 -0500

    update build script

--- frontend/build-frontend.sh
+++ frontend/build-frontend.sh
@@ -3,6 +3,6 @@ apt update -y
 apt-get install nodejs npm -y
 npm i --unsafe-perm --allow-root -g npm@latest expo-cli@latest
 npm install -y
-npx expo export:web -y
+npx expo export:web
 rm -rf /usr/share/nginx/html/*
 cp ./web-build /usr/share/nginx/html
