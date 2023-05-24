e8c0a79aa2b0bde730fdb8bcdf67328e62b9455b
Nokondi <hail.nothing@gmail.com>
2023-05-24 13:18:54 -0500

    update docker-compose to use .env variables

--- frontend/build-frontend.sh
+++
@@ -1,8 +0,0 @@
-apt upgrade -y
-apt update -y
-apt-get install nodejs npm -y
-npm i --unsafe-perm --allow-root -g npm@latest expo-cli@latest
-npm install -y
-npx expo export:web
-rm -rf /usr/share/nginx/html/*
-cp ./web-build /usr/share/nginx/html
