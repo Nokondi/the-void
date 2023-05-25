4976886122bb5026d31a1d465bf4de57cf6a892e
Nokondi <hail.nothing@gmail.com>
2023-05-25 10:07:44 -0500

    update backend for https

---
+++ backend/gunicorn.conf.py
@@ -0,0 +1,5 @@
+worker_class = "eventlet"
+workers = 1 
+bind = "0.0.0.0:8000"
+certfile = "/etc/letsencrypt/live/into-the-void.app/fullchain.pem" 
+keyfile = "/etc/letsencrypt/live/into-the-void.app/privkey.pem"
